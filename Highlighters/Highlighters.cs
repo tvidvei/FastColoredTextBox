45c36dbb1cd93e46a2fa912ae7d157b4da72c2f2
Tor Vidvei <tor.vidvei@gabler.no>
2023-02-19 01:28:26 +0100

    Added Highlighters library to be used with PowerfulSample

--- Highlighters/Highlighters.cs
+++ Highlighters/Highlighters.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using FastColoredTextBoxNS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Highlighters
 {
-    public class Class1
-    {
+    [SyntaxHighlighter(Name = "CSharp3")]
+    public class CSharp2SyntaxHighlighter : SyntaxHighlighter {
+
+        public CSharp2SyntaxHighlighter(params object[] args) : base("CSharp3") {
+        }
+
     }
+
 }
